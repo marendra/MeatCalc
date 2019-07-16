8c83c58da6abb270663db529e65d8ac25f557287
Arif Marendra <marendra79@yahoo.com>
2019-07-16 10:53:45 +0700

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDj1CynbuNfiuU7H6bHM2VcDvr2K414zx4",
+  authDomain: "kalkulator-daging.firebaseapp.com",
+  databaseURL: "https://kalkulator-daging.firebaseio.com",
+  projectId: "kalkulator-daging",
+  storageBucket: "",
+  messagingSenderId: "755935882759",
+  appId: "1:755935882759:web:227445fa0d43fed4"
+};
