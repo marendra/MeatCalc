dd69697118f096f4c616b49645821d3451522fe7
Arif Marendra <marendra79@yahoo.com>
2019-07-16 12:14:21 +0700

    adding route

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Container from "@material-ui/core/Container";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HelpIcon from '@material-ui/icons/Help';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import ThumbDown from '@material-ui/icons/ThumbDown';
-import ThumbUp from '@material-ui/icons/ThumbUp';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
+
+import utama from "./pages/utama";
+import slotA from ".";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,13 +33,11 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   },
   stickToBottom: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0,
-  },
+    width: "100%",
+    position: "fixed",
+    bottom: 0
+  }
 }));
-
-import "./styles.css";
 
 function App() {
   const classes = useStyles();
@@ -55,28 +57,28 @@ function App() {
           </Toolbar>
         </AppBar>
       </div>
-      <main >
-     
-
+      <main>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={utama} />
+          </Switch>
+        </Container>
       </main>
       <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="scrollable"
-          scrollButtons="on"
-          indicatorColor="primary"
-          textColor="primary"
-          className={classes.stickToBottom}
-          variant="fullWidth"
-          
-        >
-          <Tab label="Item One" icon={<PhoneIcon />} />
-          <Tab label="Item Two" icon={<FavoriteIcon />} />
-          <Tab label="Item Three" icon={<PersonPinIcon />} />
-          <Tab label="Item Four" icon={<HelpIcon />} />
-          <Tab label="Item Five" icon={<ShoppingBasket />} />
-          
-        </Tabs>
+        value={value}
+        onChange={handleChange}
+        scrollButtons="on"
+        indicatorColor="primary"
+        textColor="primary"
+        className={classes.stickToBottom}
+        variant="fullWidth"
+      >
+        <Tab label="Item One" icon={<PhoneIcon />} component={Link} to="/" />
+        <Tab label="Item Two" icon={<FavoriteIcon />} />
+        <Tab label="Item Three" icon={<PersonPinIcon />} />
+        <Tab label="Item Four" icon={<HelpIcon />} />
+        <Tab label="Item Five" icon={<ShoppingBasket />} />
+      </Tabs>
     </BrowserRouter>
   );
 }
---
+++ src/pages/slotA.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function slotA() {
+  return (
+    <div>
+      <h1>Slot A</h1>
+    </div>
+  );
+}
